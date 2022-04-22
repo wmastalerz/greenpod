ee2f2cfb98c358d2440ec64620c5dddcff82cd31
Wojtek Mastalerz <wmastalerz@tlen.pl>
2022-04-22 08:03:34 +0200

    remove unnecessery message

--- 2resource.py
+++ 2resource.py
@@ -10,7 +10,6 @@ sumcpu=0
 pod_nores_count=0
 suffix = {"m": 0.001}
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
     #print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
